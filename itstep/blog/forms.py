b446b901696ae789df295e9d0f3ae359b5b29ac9
Ivan <user-email@go.uk>
2024-08-20 16:35:46 +0300

    init - crud for model post, form wit crispy [13]

--- itstep/blog/forms.py
+++ itstep/blog/forms.py
@@ -1,11 +1,12 @@
 from django import forms
-from .models import Comment, Tag
+from .models import Comment, Tag, Post
 from django.core.exceptions import ValidationError
 
 
 def validate_lowercase(value):
-    if value.lower() != value:
+    if value.lower()!=value:
         raise ValidationError(f"{value} is not lowercase.")
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -21,8 +22,7 @@ class CommentForm(forms.ModelForm):
 
 
 class TagForm(forms.ModelForm):
-    name = forms.CharField(label='Title for tag',
-                           help_text="Enter your tag",
+    name = forms.CharField(label='Title for tag', help_text="Enter your tag",
                            widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Enter new tag"}),
                            # validators=[validate_lowercase]
                            )
@@ -31,8 +31,7 @@ class TagForm(forms.ModelForm):
         model = Tag
         fields = ['name']
 
-        # fields = "__all__"
-        # widgets = {"name": forms.TextInput(attrs={"placeholder": "-Enter new tag-"})}
+        # fields = "__all__"  # widgets = {"name": forms.TextInput(attrs={"placeholder": "-Enter new tag-"})}
 
     def clean_name(self):
         value = self.cleaned_data['name']
@@ -44,3 +43,22 @@ class TagForm(forms.ModelForm):
             self.add_error(None, "The total number of chars  must be 3 or greate.")
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'status', 'tags', 'category', 'image', "publish"]
+        widgets = {'body': forms.Textarea(attrs={'rows': 5, 'cols': 40}),
+            'image': forms.ClearableFileInput(attrs={'multiple': False}),
+            'publish': forms.DateTimeInput(attrs={'type': 'datetime-local'})}
+
+    def clean_tags(self):
+        tags = self.cleaned_data.get('tags')
+
+        if not tags:
+            raise forms.ValidationError("Будь ласка, виберіть принаймні один тег.")
+
+        # Додаткові перевірки, якщо потрібно
+        if len(tags) > 5:  # Наприклад, обмежити до 5 тегів
+            raise forms.ValidationError("Можна вибрати не більше 5 тегів.")
+
+        return tags
